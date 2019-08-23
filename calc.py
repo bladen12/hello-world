59f127e2342c40c49d7358a6e8e09a08d7006661
Bladen Cloud <bladenbhujel96@gmail.com>
2019-08-23 21:26:56 +0545

    Numbers and functions

---
+++ calc.py
@@ -0,0 +1,22 @@
+def add(x, y):
+   add x + y
+
+def sub(x, y):
+   subtract x - y
+
+def mult(x, y):
+   return x * y
+
+def div(x, y):
+  return x / y
+
+def mod(x, y):
+  modulus x % y
+
+
+add (1, 2)
+sub(90, 90)
+mult(5, 6)
+div(8, 2)
+mod(120, 5)
+
